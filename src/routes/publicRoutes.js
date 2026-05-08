c9113ca2cd2da99fdfc8c069252e801fb08c0fa9
nileshnishad <nnishad83@gmail.com>
2026-05-09 00:38:36 +0530

    skills api

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,12 +1,13 @@
 import  express from 'express';
 
-import { getSkills, getAllSkills, addSkills, updateSkill, getDashboardStats } from '../controllers/publicController.js';
+import { getSkills, getAllSkills, getAllSkillsName, addSkills, updateSkill, getDashboardStats } from '../controllers/publicController.js';
 import { authenticateToken, isAdmin } from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
 
 router.get('/skills', getSkills);
+router.get('/getallskillsname', getAllSkillsName);
 router.get('/getskills',authenticateToken, getAllSkills);
 router.post('/addskills', authenticateToken, addSkills);
 router.put('/updateskills/:skillId', authenticateToken, updateSkill);
